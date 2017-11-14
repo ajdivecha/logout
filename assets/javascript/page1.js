71b31da4405d41fdb96ae582fdd5bbadd0523bbe
Adrian Divecha <ajdivecha@MacBook-Pro-2.local>
2017-11-14 12:12:36 -0800

    working

---
+++ assets/javascript/page1.js
@@ -0,0 +1,45 @@
+var number = 60;
+var intervalId;
+var clicked = false;
+
+$("#picture").on("click", function(){
+
+if(clicked === false){
+
+run()	
+
+clicked = true;
+}
+
+});
+
+function run(){
+
+intervalId = setInterval(decrement, 1000);
+
+}
+
+function decrement(){
+
+number--;
+
+$("#timer").html("<p>"+ number + "</p>");
+
+if (number === 0){
+
+stop();
+
+alert("Time Up!");
+
+}
+
+}
+
+function stop(){
+
+clearInterval(intervalId);
+
+}
+
+
+
